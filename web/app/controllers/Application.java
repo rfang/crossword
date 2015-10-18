eded33367daa739fc7fda28517c84f225be56bb5
Bryan Ngo <bngo92@gmail.com>
2015-10-18 00:20:44 -0700

    Hello JSON

--- web/app/controllers/Application.java
+++ web/app/controllers/Application.java
@@ -1,14 +1,19 @@
 package controllers;
 
 import play.*;
+import play.libs.Json;
 import play.mvc.*;
 
 import views.html.*;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Application extends Controller {
 
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        ObjectNode result = Json.newObject();
+        result.put("exampleField1", "foobar");
+        result.put("exampleField2", "Hello world!");
+        return ok(result);
     }
 
 }
